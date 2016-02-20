352c385f21c10b141b9ac935cfe2498a2d0e3133
CuteITGuy <grisetroseciel@gmail.com>
2016-02-20 20:06:41 +0700

    First commit: finish the View part

---
+++ AutoLoginModels/LoginInfo.cs
@@ -0,0 +1,28 @@
+﻿using CB.Model.Common;
+
+
+namespace AutoLoginModels
+{
+    public class LoginInfo: ObservableObject
+    {
+        #region Fields
+        private string _password;
+        private string _user;
+        #endregion
+
+
+        #region  Properties & Indexers
+        public string Password
+        {
+            get { return _password; }
+            set { SetProperty(ref _password, value); }
+        }
+
+        public string User
+        {
+            get { return _user; }
+            set { SetProperty(ref _user, value); }
+        }
+        #endregion
+    }
+}
